3271f32ab5383b505b21e819d7cd1f91e46fb1eb
poimorph <suhayb.saleh@udc.es>
2026-06-18 22:47:15 +0200

    Set up the UI theme

--- app/src/main/java/com/example/colocmeal/ui/theme/Theme.kt
+++ app/src/main/java/com/example/colocmeal/ui/theme/Theme.kt
@@ -18,26 +18,25 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = md_primary,
+    primaryContainer = md_primaryContainer,
+    secondary = md_secondary,
+    secondaryContainer = md_secondaryContainer,
+    tertiary = md_tertiary,
+    tertiaryContainer = md_tertiaryContainer,
+    error = md_error,
+    surface = md_surface,
+    surfaceContainer = md_surfaceContainer,
+    outline = md_outline,
+    outlineVariant = md_outlineVariant,
+    onSurface = md_onSurface,
 )
 
 @Composable
 fun ColocMealTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
