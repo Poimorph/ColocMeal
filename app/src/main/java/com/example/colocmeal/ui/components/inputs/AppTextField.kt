43023665364a04c0a0dc6e2973da5869c01d3249
poimorph <suhayb.saleh@udc.es>
2026-06-20 17:22:58 +0200

    AppTextField improved

--- app/src/main/java/com/example/colocmeal/ui/components/inputs/AppTextField.kt
+++ app/src/main/java/com/example/colocmeal/ui/components/inputs/AppTextField.kt
@@ -1,5 +1,7 @@
 package com.example.colocmeal.ui.components.inputs
 
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,5 +24,25 @@ fun AppTextField(
     maxLines: Int = Int.MAX_VALUE
 ){
     // TODO : Visual Aspect
-    OutlinedTextField(value, onValueChange, modifier, label={ Text(label) }, visualTransformation=visualTransformation)
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        label = { Text(text = label) },
+        supportingText = supportingText?.let { text ->
+            { Text(text = text) }
+        },
+        isError = isError,
+        trailingIcon = trailingIcon?.let { icon ->
+            {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = null
+                )
+            }
+        },
+        visualTransformation = visualTransformation,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        maxLines = maxLines
+    )
 }
